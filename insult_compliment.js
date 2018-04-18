3d46d0b87450ea9f12bc1d17f3016c29340c85b4
Molly Burns <Molly@Kathryns-MacBook-Pro.local>
2018-04-18 19:48:01 -0400

    variable names

--- insult_compliment.js
+++ insult_compliment.js
@@ -2,13 +2,13 @@ const http = require("http");
 // 65535 TCP ports
 const PORT1 = 7500;
 var meanThings = ["You are dumb!", "You have no class.", "You can't do simple math.", "You're an ugly pile of crap!"];
-var server = http.createServer(function(req,res) {
+var server1 = http.createServer(function(req,res) {
     //process the request and respond
     var mean = meanThings[Math.floor(Math.random()*meanThings.length)];
     res.end(mean)
 });
 //listen for inbound connections
-server.listen(PORT1, function(err){
+server1.listen(PORT1, function(err){
     if (err){
         console.log.error
     }else{
@@ -19,13 +19,13 @@ const PORT2 = 7000;
 var niceThings = ["You are so beautiful!", "You're a hottie!", "You're really smart", "I think your tractor's sexy", "I love your style"];
 
 
-var server = http.createServer(function(req,res) {
+var server2 = http.createServer(function(req,res) {
     //process the request and respond
     var nice = niceThings[Math.floor(Math.random()*niceThings.length)];
     res.end(nice)
 });
 //listen for inbound connections
-server.listen(PORT2, function(err){
+server2.listen(PORT2, function(err){
     if (err){
         console.log.error
     }else{
